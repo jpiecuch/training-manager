d631ca471ddc199ab47b56beaf3ad0ec94bb0341
jpiecuch <jakub.piecuch@jakubpiecuch.pl>
2015-02-08 09:41:09 +0100

    Plan starter UI

--- src/main/webapp/resources/js/services/plan-service.js
+++ src/main/webapp/resources/js/services/plan-service.js
@@ -59,7 +59,7 @@ MetronicApp.service('planService', function($q, $http, phaseService, formValidat
                 name: data ? data.data.name : null,
                 goal: data ? data.data.goal : null,
                 phases: [],
-                editable: data.editable,
+                editable: data ? data.editable : true,
                 isValid: function() {
                     return me.isValid(this);
                 },
