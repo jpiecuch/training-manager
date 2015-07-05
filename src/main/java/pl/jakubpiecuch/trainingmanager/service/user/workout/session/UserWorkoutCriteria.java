f6ec4e98652cff1449bd09cc3210e3a0b688a0a0
jpiecuch <jakub.piecuch@jakubpiecuch.pl>
2015-07-05 10:22:52 +0200

    Some changes accepted in mind

--- src/main/java/pl/jakubpiecuch/trainingmanager/service/user/workout/session/UserWorkoutCriteria.java
+++ src/main/java/pl/jakubpiecuch/trainingmanager/service/user/workout/session/UserWorkoutCriteria.java
@@ -15,7 +15,6 @@ public class UserWorkoutCriteria extends Criteria<UserWorkoutCriteria> {
 
     public UserWorkoutCriteria(String lang) {
         super("u", "UserWorkout", lang);
-        addJoin("LEFT JOIN FETCH " + alias + ".executions e");
         addJoin("LEFT JOIN FETCH " + alias + ".workout w");
         addJoin("LEFT JOIN FETCH w.phase ph");
         addJoin("LEFT JOIN FETCH ph.plan p");
